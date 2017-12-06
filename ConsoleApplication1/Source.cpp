a7bfeab32cbd8199af8d2365b6af1b81eb7c6875
ХандогинР <@227_12>
2017-12-06 09:41:01 +0600

    Add project files.

---
+++ ConsoleApplication1/Source.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+using namespace std; 
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int nz = 0;
+start:
+	
+		cout << "Ведите номер задания";
+		cin >> nz;
+		if (nz == 1)
+		{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		}
+		else if (nz >= 0)
+		{
+			cout << "Такого задание нету , повторите пожалуйста попытку \n";
+
+		}
+	system("pause");
+	//getch();
+	return 0;
+}
